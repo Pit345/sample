1fd864e06c3f37c8ad4b0356840e495151557099
Peter3452 <p1312402@gmail.com>
2023-03-27 23:00:02 +0300

    intermediate commit

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,18 @@
+class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @micropost = current_user.microposts.create(micropost_params)
+    if @micropost.save
+      flash[:succes] = 'Micropost created!'
+      redirect_to root_path
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  private
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+end
