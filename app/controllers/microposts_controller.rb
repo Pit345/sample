3252454debbda292e656428edc2e735c6b16f5b4
Peter3452 <p1312402@gmail.com>
2023-03-31 15:15:55 +0300

    +++

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,4 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
 
   def create
     @micropost = current_user.microposts.create(micropost_params)
@@ -9,6 +8,11 @@ class MicropostsController < ApplicationController
     else
       render 'static_pages/home'
     end
+  end
+
+  def destroy
+    @micropost.destroy
+    #redirect_to user_path
   end
 
   private
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,15 +6,4 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
-
-  def current_user?(user)
-    user == current_user
-  end
-
-  def logged_in_user
-    if !logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to root_path
-    end
-  end
 end
